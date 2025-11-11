171591e46801cd964e88e0790ab5da85c51a4886
begli.amanov <beglee.amanov@gmail.com>
2025-11-11 23:42:20 +0200

    refactor

--- src/test/kotlin/easy/ValidAnagramTest.kt
+++ src/test/kotlin/easy/ValidAnagramTest.kt
@@ -1,7 +1,6 @@
 package easy
 
 
-import junit.framework.TestCase.assertEquals
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
